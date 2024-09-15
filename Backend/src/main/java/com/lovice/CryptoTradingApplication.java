1e16d74d5a457b31a1d18549601b8056808301ec
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-09-15 11:25:18 -0400

    Spring Boot project intialization, Spring Web, MySQL, Spring JPA, Lombok, Spring Security are added in the initial setup

---
+++ Backend/src/main/java/com/lovice/CryptoTradingApplication.java
@@ -0,0 +1,13 @@
+package com.lovice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTradingApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/lovice/CryptoTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
